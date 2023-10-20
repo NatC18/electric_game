e3ded53c086f1de8566ad02203594283a6a40083
Natalia Correal <natalia.correal@uc.cl>
2023-10-20 15:26:57 -0300

    map circles

--- codefest/map_circles.gd
+++ codefest/map_circles.gd
@@ -1,8 +1,17 @@
 extends Node2D
 
+
+
 var rotation_angle = 5
-var angle_from = 75
-var angle_to = 195
+var angle_from = 0
+var angle_to = 360
+var radius = 80
+
+var points = []
+
+
+
+
 
 func draw_circle_arc_poly(center, radius, angle_from, angle_to, color):
 	var nb_points = 32
@@ -16,23 +25,30 @@ func draw_circle_arc_poly(center, radius, angle_from, angle_to, color):
 	draw_polygon(points_arc, colors)
 
 func _draw():
-	var center = Vector2(200, 200)
-	var radius = 80
-	var color = Color(1.0, 0.0, 0.0)
-	draw_circle_arc_poly( center, radius, angle_from, angle_to, color )
+	
+	var color = Color(1.0, 0.0, 0.0, 0.1)
+	radius += 0.1
+	
+	for i in range(len(points)):
+		var center = Vector2(points[i] * 50, 200)
+		draw_circle_arc_poly( center, radius, angle_from, angle_to, color )
 	
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	RenderingServer.set_default_clear_color(Color.WHITE)
+	
+	for n in range(8):
+		points.push_back(n)
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	angle_from += rotation_angle
-	angle_to += rotation_angle
+#	angle_from += rotation_angle
+#	angle_to += rotation_angle
 
 	# We only wrap angles when both of them are bigger than 360.
-	if angle_from > 360 and angle_to > 360:
-		angle_from = wrapf(angle_from, 0, 360)
-		angle_to = wrapf(angle_to, 0, 360)
+#	if angle_from > 360 and angle_to > 360:
+#		angle_from = wrapf(angle_from, 0, 360)
+#		angle_to = wrapf(angle_to, 0, 360)
 	queue_redraw()
